b5179da153d3152ee9d7ffea0f08e3bbee202bb0
soldesk <soldesk@DESKTOP-48UJ7T0>
2020-07-02 18:00:53 +0900

    java24

--- Java24/src/min/GenericMethod.java
+++ Java24/src/min/GenericMethod.java
@@ -16,7 +16,8 @@ public class GenericMethod {
 		Point<Integer, Double> p1=new Point<Integer, Double>(0,0.0);  //x좌표
 		Point<Integer, Double> p2=new Point<>(10,10.0);  //y좌표
 		double rect=GenericMethod.<Integer, Double>makeRec(p1, p2);
-		System.out.println("두 점으로 만들어진 사각형의 넓이는 "+rect + "입니다.");
+		System.out.println("두 점으로 만들어진 사각형의 넓이는 "+rect + "입니다..");
+		System.out.println("오늘은 7월 2일");
 		
 	}
 
---
+++ Java24/src/min/today.java
@@ -0,0 +1,7 @@
+package min;
+
+public class today {
+public static void main(String[] args) {
+	System.out.println("today");
+}
+}
